8962f68c5a4f7f01dee6ecbe1bb8032779c0ab57
Ansxy <ansarfadillah17@gmail.com>
2021-06-28 15:44:16 +0800

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
+/* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
@@ -18,4 +19,6 @@ const init = async () => {
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
+// eslint-disable-next-line linebreak-style
+// eslint-disable-next-line linebreak-style
 init();
